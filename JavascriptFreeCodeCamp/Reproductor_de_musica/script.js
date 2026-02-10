f715876c7942eb4f0a9606fa8159ce3d8d326651
Jose Valduz <jose.valduz@unet.edu.ve>
2026-02-10 14:26:46 -0400

    Agregar funcionalidad para reproducir canciones al hacer clic en el botón de reproducción

--- JavascriptFreeCodeCamp/Reproductor_de_musica/script.js
+++ JavascriptFreeCodeCamp/Reproductor_de_musica/script.js
@@ -95,8 +95,10 @@ const playSong = (id) => {
   } else {
     audio.currentTime = userData?.songCurrentTime;
   }
-
   userData.currentSong = song;
+  playButton.classList.add("playing");
+
+  audio.play();
 };
 
 const renderSongs = (array) => {
@@ -104,7 +106,7 @@ const renderSongs = (array) => {
     .map((song) => {
       return `
       <li id="song-${song.id}" class="playlist-song">
-      <button class="playlist-song-info">
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
           <span class="playlist-song-title">${song.title}</span>
           <span class="playlist-song-artist">${song.artist}</span>
           <span class="playlist-song-duration">${song.duration}</span>
@@ -120,6 +122,14 @@ const renderSongs = (array) => {
 
   playlistSongs.innerHTML = songsHTML;
 };
+
+playButton.addEventListener("click", () => {
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    playSong(userData?.currentSong.id);
+  }
+});
 
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
